8286bd72c5526e38ea97377c871ae01917bb3d4e
farazshaon <farazshaon0@gmail.comm>
2022-10-18 10:07:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [rating, setRating] = useState([
+    {
+      id: 1,
+      text: 'Hello'
+    },
+    {
+      id: 1,
+      text: 'Hello'
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     
+     {rating.map((item) => (
+      <div>{item.text}</div>
+     ))}
     </div>
   );
 }
